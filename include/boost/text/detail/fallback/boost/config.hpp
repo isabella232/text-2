96ba8c5225d3b6db01f727377192d69a75d81a52
Victor Zverovich <victor.zverovich@gmail.com>
2019-09-14 10:07:08 -0700

    Detect relaxed constexpr

--- include/boost/text/detail/fallback/boost/config.hpp
+++ include/boost/text/detail/fallback/boost/config.hpp
@@ -1,6 +1,10 @@
 #ifndef TEXT_BOOST_CONFIG_HPP
 #define TEXT_BOOST_CONFIG_HPP
 
-#define BOOST_CXX14_CONSTEXPR constexpr
+#if defined(__has_feature) && __has_feature(cxx_relaxed_constexpr)
+# define BOOST_CXX14_CONSTEXPR constexpr
+#else
+# define BOOST_CXX14_CONSTEXPR
+#endif
 
 #endif  // TEXT_BOOST_CONFIG_HPP
